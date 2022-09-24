d1d79e8cc54465f5989e29a4e7f96054b91590c1
Heber Daniel Maluendas Lizarazo <111641836+Maluendas0510@users.noreply.github.com>
2022-09-24 00:56:52 -0500

    Add files via upload

---
+++ proyecto_alimentacion/codigo.py
@@ -0,0 +1,26 @@
+# lista de aliemtos 
+
+print("----------------------")
+print("--- alimentacion------")
+print("----------------------")
+
+
+#input 
+
+print("seleccione la opcion que desea ver:")
+print("--1.desayuno--")
+print("--2.almuerzo--")
+print("--3.comida--")
+
+d=int(input("digite que recetas desea ver:"))
+
+
+#processing
+if("desayunos"):
+    if(d==1):
+        print("\n---DESAYUNOS--- \n1.-Omelet con queso y hierbas- \nIngredientes: 3 huevos batidos, 1 cucharada de hierbas de olor, 1 cucharada de mantequilla sin sal, 50 gramos de queso .\nModo de preparar: se mezcla todos los ingredientes excepto la mantequilla y el queso.calienta la mantequilla en un sartén y cuando esté caliente agrega la mezcla.Espera hasta que se forme el omelet, entonces coloca el queso encima y dobla por la mitad, se cocina por un minuto hasta que se derrita el quesoDisfruta.")
+    elif(d==1):
+        print("\n2.-Panquecas de plátano- \nIngredientes: Tres plátanos hechos puré,100 gramos de mantequilla, 1 cucharada de miel.\nPreparación: forma las panquecas con el puré de plátano.Una vez formadas, fríelas en la mantequilla una por una, por alrededor de dos minutos.acompaña con miel y disfruta.")
+    elif(d==1):
+        print("\n3.--Batido de papaya y piña-- \nIngredientes : \n1 taza, (225 gramos) de piña en trozos,media taza (120 gramos) de papaya en trozos, media taza (125 ml) de cubos de hielo,60 ml de miel.\nPreparación: Vierte todos los ingredientes en la licuadora y licúa hasta que se mezclen.Sirve inmediatamente.")
+    
